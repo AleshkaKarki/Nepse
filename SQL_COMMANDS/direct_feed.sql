9dac0ee75e057763aa3e40222d1f9f54db72a40a
aleshka <aleshkakarki@gmail.com>
2024-01-22 13:00:15 +0545

    direct feed

---
+++ SQL_COMMANDS/direct_feed.sql
@@ -0,0 +1,45 @@
+--creating table  user details
+CREATE TABLE user details(
+    DOB DATE,
+    address VARCHAR(50),
+    gender VARCHAR(30),
+    user_id INT(11),
+    fullname VARCHAR(30),
+    phonenumber INT(11)
+    );
+
+INSERT into user(user_id, username, password, email, user_role)
+VALUES
+	(123, 'Alish', 'music', 'alishk@gmail.com', 'user'),
+	(456, 'Alisha', 'married', 'alishak@gmail.com', 'user'),
+	(789, 'Abishek', 'london', 'abishekk@gmail.com', 'user'),
+	(999, 'Aleshka', 'student', 'aleshkak@gmail.com', 'user'),
+    (345, 'Aayushma', 'dental', 'aayushmak@gmail.com', 'user'),
+	(546, 'Sneha', 'mbbs', 'snehak@gmail.com', 'user'),
+	(890, 'Rasana', 'nursing', 'rasanak@gmail.com', 'user'),
+	(134, 'Soniya', 'helping', 'soniyak@gmail.com', 'user'),
+    (378, 'Alex', 'trinity', 'alexk@gmail.com', 'user'),
+	(989, 'Arnav', 'school', 'arnavk@gmail.com', 'user'),
+	(678, 'Aaravi', 'funnyone', 'aaravik@gmail.com', 'user'),
+	(279, 'Aashreeva', 'youngest', 'aashreevak@gmail.com', 'user'),
+    (659, 'Rohit', 'aussie', 'rohitk@gmail.com', 'user'),
+	(696, 'Rohan', 'drummer', 'rohank@gmail.com', 'user'),
+	(879, 'Aaradhya', 'dancer', 'aaradhyak@gmail.com', 'user'),
+	(987, 'Calvin', 'stupid', 'calvink@gmail.com', 'user'),
+    (111, 'Bless', 'funnyname', 'blessk@gmail.com', 'user'),
+	(222, 'Sokheen', 'mountains', 'sokheenk@gmail.com', 'user'),
+	(333, 'Priyanka', 'indie', 'priyankak@gmail.com', 'user'),
+	(444, 'Samridha', 'alwayshigh', 'samridhak@gmail.com', 'user'),
+    (555, 'Christina', 'stubborn', 'christinak@gmail.com', 'user'),
+	(666, 'Shivam', 'cards', 'shivamk@gmail.com', 'user'),
+	(777, 'Aashwini', 'hatest', 'aashwinik@gmail.com', 'user'),
+	(888, 'Manisha', 'favorite', 'manishak@gmail.com', 'user'),
+    (909, 'Ramesh', 'tvhero', 'rameshk@gmail.com', 'user'),
+	(101, 'Rikesh', 'chef', 'rikeshk@gmail.com', 'user'),
+	(202, 'Nikesh', 'untitled', 'nikeshk@gmail.com', 'user'),
+	(303, 'Sabin', 'coolest', 'sabink@gmail.com', 'user'),
+    (404, 'Puggy', 'mylove', 'puggyk@gmail.com', 'user'),
+	(505, 'Joy', 'angry', 'joyk@gmail.com', 'user'),
+	(606, 'Candy', 'socute', 'candyk@gmail.com', 'user'),
+	(707, 'Fucche', 'dangerous', 'fuchhek@gmail.com', 'user'),
+    (808, 'Kanchi', 'sweet', 'kanchik@gmail.com', 'user');
